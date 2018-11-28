2d8a6ebd2d5f820bd0c79a5407e274e12e91f6b8
Charmy <desai.cha@husky.neu.edu>
2018-11-28 18:06:55 -0500

    Angular code for registration and mongo DB set up

---
+++ login/BeatShareApp/src/app/user.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private _http:HttpClient) { }
+
+//rest api call 
+  register(body:any){
+    return this._http.post('http://127.0.0.1:3000/users/register',body,{
+      observe:'body',
+      headers:new HttpHeaders().append('Content-Type','application/json')
+    });
+  }
+  //rest api call
+  login(body:any){
+    return this._http.post('http://127.0.0.1:3000/users/login',body,{
+      observe:'body',
+      withCredentials:true,
+      headers:new HttpHeaders().append('Content-Type','application/json')
+    });
+  }
+  //rest api call
+  user(){
+    return this._http.get('http://127.0.0.1:3000/users/user',{
+      observe:'body',
+      withCredentials:true,
+      headers:new HttpHeaders().append('Content-Type','application/json')
+    })
+  }
+  logout(){
+    return this._http.get('http://127.0.0.1:3000/users/logout',{
+      observe:'body',
+      withCredentials:true,
+      headers:new HttpHeaders().append('Content-Type','application/json')
+    })
+  }
+}

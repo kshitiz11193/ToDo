0770d1a9514cc78401a0f28226cb00d660bf7926
Charmy <desai.cha@husky.neu.edu>
2018-12-02 14:18:21 -0500

    changes suggested by TA post review implemented

---
+++ expressapp/routes/users.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+var passport = require('passport');
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/register', function (req, res, next) {
+  addToDB(req, res);
+});
+
+
+async function addToDB(req, res) {
+
+  var user = new User({
+    email: req.body.email,
+    username: req.body.username,
+    password: User.hashPassword(req.body.password),
+    creation_dt: Date.now()
+  });
+ 
+//await function to give the feel it is a synchronous call where as internally it is an asynchronous call.
+  try {
+    doc = await user.save();  //saves the user to mongoDB
+    return res.status(201).json(doc);
+  }
+  catch (err) {
+    return res.status(501).json(err);
+  }
+}
+router.post('/login',function(req,res,next){
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return res.status(501).json(err); }
+    if (!user) { return res.status(501).json(info); }
+    //logIn function is provoded by passport, if user exists check if any error, if not sign the user in
+    req.logIn(user, function(err) {
+      if (err) { return res.status(501).json(err); }
+      return res.status(200).json({message:'Login Success'});
+    });
+  })(req, res, next);
+});
+
+router.get('/user',isValidUser,function(req,res,next){
+  return res.status(200).json(req.user);
+});
+
+router.get('/logout',isValidUser, function(req,res,next){
+  req.logout();
+  return res.status(200).json({message:'Logout Success'});
+})
+
+function isValidUser(req,res,next){
+  if(req.isAuthenticated()) next();
+  else return res.status(401).json({message:'Unauthorized Request'});
+}
+module.exports = router;
